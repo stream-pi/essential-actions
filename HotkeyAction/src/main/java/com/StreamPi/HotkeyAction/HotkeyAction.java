d391854886c796b365476dc89536d4dd946d74bd
rnayabed <debayansutradhar3@gmail.com>
2021-01-21 20:46:31 +0530

    Added Hotkey support for OBS, Changed Robot from AWT to JavaFX

--- HotkeyAction/src/main/java/com/StreamPi/HotkeyAction/HotkeyAction.java
+++ HotkeyAction/src/main/java/com/StreamPi/HotkeyAction/HotkeyAction.java
@@ -7,11 +7,14 @@ import com.StreamPi.Util.Alert.StreamPiAlert;
 import com.StreamPi.Util.Alert.StreamPiAlertType;
 import com.StreamPi.Util.Exception.MinorException;
 import com.StreamPi.Util.Version.Version;
-import static java.awt.event.KeyEvent.*;
+import javafx.application.Platform;
+import javafx.scene.input.KeyCode;
+import javafx.scene.robot.Robot;
 
-import java.awt.*;
 import java.net.URI;
 import java.util.ArrayList;
+
+import static javafx.scene.input.KeyCode.*;
 
 public class HotkeyAction extends NormalAction {
 
@@ -38,7 +41,7 @@ public class HotkeyAction extends NormalAction {
 
     @Override
     public void initAction() throws Exception {
-        robot = new Robot();
+        Platform.runLater(()->robot = new Robot());
     }
 
     @Override
@@ -59,14 +62,15 @@ public class HotkeyAction extends NormalAction {
         );
     }
 
-    public void press(String[] characters) {
+    public void press(String[] characters) throws InterruptedException {
 
-        ArrayList<Integer> pressedChars = new ArrayList<>();
+        ArrayList<KeyCode> pressedChars = new ArrayList<>();
 
         try{
             for (String s : characters) {
-                int vkValue = getVKValue(s.trim());
+                KeyCode vkValue = getKeyCodeValue(s.trim());
                 press(vkValue);
+                Thread.sleep(50);
                 pressedChars.add(vkValue);
             }
         }
@@ -76,171 +80,170 @@ public class HotkeyAction extends NormalAction {
         }
     }
 
-    private void releaseCharacters(ArrayList<Integer> chars)
-    {
-        for(Integer c : chars)
+    private void releaseCharacters(ArrayList<KeyCode> chars) throws InterruptedException {
+        for(KeyCode c : chars)
         {
             release(c);
+            Thread.sleep(50);
         }
     }
 
-    public int getVKValue(String character) {
+    public KeyCode getKeyCodeValue(String character) {
         switch (character) {
-            case "A": return VK_A; 
-            case "B": return VK_B; 
-            case "C": return VK_C; 
-            case "D": return VK_D; 
-            case "E": return VK_E; 
-            case "F": return VK_F; 
-            case "G": return VK_G; 
-            case "H": return VK_H; 
-            case "I": return VK_I; 
-            case "J": return VK_J; 
-            case "K": return VK_K; 
-            case "L": return VK_L; 
-            case "M": return VK_M; 
-            case "N": return VK_N; 
-            case "O": return VK_O; 
-            case "P": return VK_P; 
-            case "Q": return VK_Q; 
-            case "R": return VK_R; 
-            case "S": return VK_S; 
-            case "T": return VK_T; 
-            case "U": return VK_U; 
-            case "V": return VK_V; 
-            case "W": return VK_W; 
-            case "X": return VK_X; 
-            case "Y": return VK_Y; 
-            case "Z": return VK_Z; 
-            case "`": return VK_BACK_QUOTE; 
-            case "0": return VK_0; 
-            case "1": return VK_1; 
-            case "2": return VK_2; 
-            case "3": return VK_3; 
-            case "4": return VK_4; 
-            case "5": return VK_5; 
-            case "6": return VK_6; 
-            case "7": return VK_7; 
-            case "8": return VK_8; 
-            case "9": return VK_9; 
-            case "-": return VK_MINUS; 
-            case "=": return VK_EQUALS; 
-            case "~": return VK_BACK_QUOTE; 
-            case "!": return VK_EXCLAMATION_MARK; 
-            case "@": return VK_AT; 
-            case "#": return VK_NUMBER_SIGN; 
-            case "$": return VK_DOLLAR; 
-            case "%": return VK_5; 
-            case "^": return VK_CIRCUMFLEX; 
-            case "&": return VK_AMPERSAND; 
-            case "*": return VK_ASTERISK; 
-            case "(": return VK_LEFT_PARENTHESIS; 
-            case ")": return VK_RIGHT_PARENTHESIS; 
-            case "_": return VK_UNDERSCORE; 
-            case "+": return VK_PLUS; 
-            case "TAB": return VK_TAB;
-            case "[": return VK_OPEN_BRACKET; 
-            case "]": return VK_CLOSE_BRACKET; 
-            case "\\": return VK_BACK_SLASH; 
-            case "{": return VK_OPEN_BRACKET; 
-            case "}": return VK_CLOSE_BRACKET; 
-            case "|": return VK_BACK_SLASH; 
-            case ";": return VK_SEMICOLON; 
-            case ":": return VK_COLON; 
-            case "\"": return VK_BACK_SLASH;
-            case ",": return VK_COMMA; 
-            case "<": return VK_LESS;
-            case ".": return VK_PERIOD; 
-            case ">": return VK_GREATER;
-            case "/": return VK_SLASH; 
-            case " ": return VK_SPACE;
+            case "A": return A;
+            case "B": return B; 
+            case "C": return C; 
+            case "D": return D; 
+            case "E": return E; 
+            case "F": return F; 
+            case "G": return G; 
+            case "H": return H; 
+            case "I": return I; 
+            case "J": return J; 
+            case "K": return K; 
+            case "L": return L; 
+            case "M": return M; 
+            case "N": return N; 
+            case "O": return O; 
+            case "P": return P; 
+            case "Q": return Q; 
+            case "R": return R; 
+            case "S": return S; 
+            case "T": return T; 
+            case "U": return U; 
+            case "V": return V; 
+            case "W": return W; 
+            case "X": return X; 
+            case "Y": return Y; 
+            case "Z": return Z; 
+            case "`": return BACK_QUOTE; 
+            case "0": return DIGIT0;
+            case "1": return DIGIT1;
+            case "2": return DIGIT2;
+            case "3": return DIGIT3;
+            case "4": return DIGIT4;
+            case "5": return DIGIT5;
+            case "6": return DIGIT6;
+            case "7": return DIGIT7;
+            case "8": return DIGIT8;
+            case "9": return DIGIT9;
+            case "-": return MINUS; 
+            case "=": return EQUALS; 
+            case "~": return BACK_QUOTE; 
+            case "!": return EXCLAMATION_MARK; 
+            case "@": return AT; 
+            case "#": return NUMBER_SIGN; 
+            case "$": return DOLLAR;
+            case "^": return CIRCUMFLEX; 
+            case "&": return AMPERSAND; 
+            case "*": return ASTERISK; 
+            case "(": return LEFT_PARENTHESIS; 
+            case ")": return RIGHT_PARENTHESIS; 
+            case "_": return UNDERSCORE; 
+            case "+": return PLUS; 
+            case "TAB": return TAB;
+            case "[": return OPEN_BRACKET; 
+            case "]": return CLOSE_BRACKET; 
+            case "\\": return BACK_SLASH; 
+            case "{": return OPEN_BRACKET; 
+            case "}": return CLOSE_BRACKET; 
+            case "|": return BACK_SLASH; 
+            case ";": return SEMICOLON; 
+            case ":": return COLON; 
+            case "\"": return BACK_SLASH;
+            case ",": return COMMA; 
+            case "<": return LESS;
+            case ".": return PERIOD; 
+            case ">": return GREATER;
+            case "/": return SLASH; 
+            case " ": return SPACE;
 
             case "WIN":
             case "WINDOWS":
             case "SUPER":
             case "COMMAND":
-                return VK_WINDOWS;
+                return WINDOWS;
 
             case "META":
-                return VK_META;    
+                return META;    
 
             case "SHIFT":
-                return VK_SHIFT;
+                return SHIFT;
 
-            case "ALT": return VK_ALT;
+            case "ALT": return ALT;
 
-            case "F1": return VK_F1;
-            case "F2": return VK_F2;
-            case "F3": return VK_F3;
-            case "F4": return VK_F4;
-            case "F5": return VK_F5;
-            case "F6": return VK_F6;
-            case "F7": return VK_F7;
-            case "F8": return VK_F8;
-            case "F9": return VK_F9;
-            case "F10": return VK_F10;
-            case "F11": return VK_F11;
-            case "F12": return VK_F12;
-            case "F13": return VK_F13;
-            case "F14": return VK_F14;
-            case "F15": return VK_F15;
-            case "F16": return VK_F16;
-            case "F17": return VK_F17;
-            case "F18": return VK_F18;
-            case "F19": return VK_F19;
-            case "F20": return VK_F20;
-            case "F21": return VK_F21;
-            case "F22": return VK_F22;
-            case "F23": return VK_F23;
-            case "F24": return VK_F24;
+            case "F1": return F1;
+            case "F2": return F2;
+            case "F3": return F3;
+            case "F4": return F4;
+            case "F5": return F5;
+            case "F6": return F6;
+            case "F7": return F7;
+            case "F8": return F8;
+            case "F9": return F9;
+            case "F10": return F10;
+            case "F11": return F11;
+            case "F12": return F12;
+            case "F13": return F13;
+            case "F14": return F14;
+            case "F15": return F15;
+            case "F16": return F16;
+            case "F17": return F17;
+            case "F18": return F18;
+            case "F19": return F19;
+            case "F20": return F20;
+            case "F21": return F21;
+            case "F22": return F22;
+            case "F23": return F23;
+            case "F24": return F24;
 
-            case "NUMPAD 0": return VK_NUMPAD0;
-            case "NUMPAD 1": return VK_NUMPAD1;
-            case "NUMPAD 2": return VK_NUMPAD2;
-            case "NUMPAD 3": return VK_NUMPAD3;
-            case "NUMPAD 4": return VK_NUMPAD4;
-            case "NUMPAD 5": return VK_NUMPAD5;
-            case "NUMPAD 6": return VK_NUMPAD6;
-            case "NUMPAD 7": return VK_NUMPAD7;
-            case "NUMPAD 8": return VK_NUMPAD8;
-            case "NUMPAD 9": return VK_NUMPAD9;
+            case "NUMPAD 0": return NUMPAD0;
+            case "NUMPAD 1": return NUMPAD1;
+            case "NUMPAD 2": return NUMPAD2;
+            case "NUMPAD 3": return NUMPAD3;
+            case "NUMPAD 4": return NUMPAD4;
+            case "NUMPAD 5": return NUMPAD5;
+            case "NUMPAD 6": return NUMPAD6;
+            case "NUMPAD 7": return NUMPAD7;
+            case "NUMPAD 8": return NUMPAD8;
+            case "NUMPAD 9": return NUMPAD9;
 
-            case "NUMBER SIGN": return VK_NUMBER_SIGN;
+            case "NUMBER SIGN": return NUMBER_SIGN;
             
 
-            case "HOME": return VK_HOME;
+            case "HOME": return HOME;
 
-            case "INSERT": return VK_INSERT;
+            case "INSERT": return INSERT;
 
-            case "PAGE UP": return VK_PAGE_UP;
-            case "PAGE DOWN": return VK_PAGE_DOWN;
-            case "END": return VK_END;
-            case "ENTER": return VK_ENTER;
-            case "DELETE": return VK_DELETE;
+            case "PAGE UP": return PAGE_UP;
+            case "PAGE DOWN": return PAGE_DOWN;
+            case "END": return END;
+            case "ENTER": return ENTER;
+            case "DELETE": return DELETE;
 
-            case "SCROLL LOCK": return VK_SCROLL_LOCK;
-            case "PRINT SCREEN": return VK_PRINTSCREEN;
-            case "PAUSE": return VK_PAUSE;
+            case "SCROLL LOCK": return SCROLL_LOCK;
+            case "PRINT SCREEN": return PRINTSCREEN;
+            case "PAUSE": return PAUSE;
 
-            case "NUM LOCK": return VK_NUM_LOCK;
+            case "NUM LOCK": return NUM_LOCK;
 
-            case "CONTROL": return VK_CONTROL;
+            case "CONTROL": return CONTROL;
 
-            case "CAPS LOCK": return VK_CAPS_LOCK;
+            case "CAPS LOCK": return CAPS;
 
-            case "NUM UP": return VK_KP_UP;
-            case "UP": return VK_UP;
+            case "NUM UP": return KP_UP;
+            case "UP": return UP;
 
-            case "NUM LEFT": return VK_KP_LEFT;
-            case "LEFT": return VK_LEFT;
+            case "NUM LEFT": return KP_LEFT;
+            case "LEFT": return LEFT;
 
-            case "ALT GRAPH": return VK_ALT_GRAPH;
+            case "ALT GRAPH": return ALT_GRAPH;
 
-            case "NUM RIGHT": return VK_KP_RIGHT;
-            case "RIGHT": return VK_RIGHT;
+            case "NUM RIGHT": return KP_RIGHT;
+            case "RIGHT": return RIGHT;
 
-            case "NUM DOWN": return VK_KP_DOWN;
-            case "DOWN": return VK_DOWN;
+            case "NUM DOWN": return KP_DOWN;
+            case "DOWN": return DOWN;
 
             default:
                 throw new IllegalArgumentException("Cannot press character " + character);
@@ -248,14 +251,14 @@ public class HotkeyAction extends NormalAction {
     }
 
     
-    private void press(int keyCode)
+    private void press(KeyCode keyCode)
     {
-        robot.keyPress(keyCode);
+        Platform.runLater(()->robot.keyPress(keyCode));
     }
 
-    private void release(int keyCode)
+    private void release(KeyCode keyCode)
     {
-        robot.keyRelease(keyCode);
+        Platform.runLater(()->robot.keyRelease(keyCode));
     }
 
     @Override
