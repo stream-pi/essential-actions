7fce4aece7c3d89f16882d7239fb99a487e3e8a4
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2021-03-27 13:54:10 +0800

    Use latest action-api in twitch whisper

--- twitch/whisper/src/main/java/module-info.java
+++ twitch/whisper/src/main/java/module-info.java
@@ -5,5 +5,5 @@ module com.stream_pi.twitch.whisperaction {
     requires com.stream_pi.twitchchatconnectaction;
     requires Java.Twirk;
 
-    provides com.stream_pi.action_api.normalaction.NormalAction with whisper.WhisperAction;
+    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with whisper.WhisperAction;
 }
