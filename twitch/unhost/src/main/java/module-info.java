ef4cde79e78b861fc4f5f0a28c2dc9204a1d8a5c
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2021-03-27 13:50:54 +0800

    Use latest action-api twitch unhost

--- twitch/unhost/src/main/java/module-info.java
+++ twitch/unhost/src/main/java/module-info.java
@@ -5,5 +5,5 @@ module com.stream_pi.twitch.unhostaction {
     requires com.stream_pi.twitchchatconnectaction;
     requires Java.Twirk;
 
-    provides com.stream_pi.action_api.normalaction.NormalAction with unhost.UnhostAction;
+    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with unhost.UnhostAction;
 }
