47d3f830186fdc7c5fe8e10ecc711678fcff7a30
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-04-02 20:54:28 +0530

    updated obs actions to run on latest action api

--- obssuite/setpreviewscene/src/main/java/module-info.java
+++ obssuite/setpreviewscene/src/main/java/module-info.java
@@ -6,5 +6,5 @@ module com.stream_pi.obssuite.setpreviewsceneaction
     requires obs.websocket.java;
     requires com.stream_pi.obssuite.motheraction;
 
-    provides com.stream_pi.action_api.normalaction.NormalAction with setpreviewscene.SetPreviewScene;
+    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with setpreviewscene.SetPreviewScene;
 }
--- obssuite/setstudiomode/src/main/java/module-info.java
+++ obssuite/setstudiomode/src/main/java/module-info.java
@@ -6,5 +6,5 @@ module com.stream_pi.obssuite.setstudiomodeaction
     requires obs.websocket.java;
     requires com.stream_pi.obssuite.motheraction;
 
-    provides com.stream_pi.action_api.normalaction.NormalAction with SetStudioMode.SetStudioMode;
+    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with SetStudioMode.SetStudioMode;
 }
