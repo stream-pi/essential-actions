f9229b0afc49a123341c68260d974ff723bbaa2b
rnayabed <debayansutradhar3@gmail.com>
2021-04-04 19:09:55 +0530

    edit build script to differentiate between prebuilt and dependencies

--- build.sh
+++ build.sh
@@ -1,132 +1,142 @@
 #!/bin/bash
 
-FOLD=../PreBuiltPlugins
+FOLD=PreBuiltPlugins
+DEPS=Dependencies
 
 hotkey() {
   pushd . || exit
   cd hotkeyaction && mvn clean -Dmaven.test.skip package
-  mv target/hotkeyaction-*.jar $FOLD/hotkeyaction.jar
+  mv target/hotkeyaction-*.jar ../$FOLD/hotkeyaction.jar
   popd || exit
 }
 
 mediakey() {
   pushd . || exit
   cd mediakeyaction && mvn clean -Dmaven.test.skip package
-  mv target/mediakeyaction-*.jar $FOLD/mediakeyaction.jar
+  mv target/mediakeyaction-*.jar ../$FOLD/mediakeyaction.jar
   popd || exit
 }
 
 obssuite() {
   pushd . || exit
   cd obssuite/mother && mvn clean install -Dmaven.test.skip package
-  mv target/obssuite_motheraction-*.jar ../$FOLD/obssuite_motheraction.jar
+  mv target/obssuite_motheraction-*.jar ../../$FOLD/obssuite_motheraction.jar
 
   cd ../setcurrentprofile && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setcurrentprofileaction-*.jar ../$FOLD/obssuite_setcurrentprofileaction.jar
+  mv target/obssuite_setcurrentprofileaction-*.jar ../../$FOLD/obssuite_setcurrentprofileaction.jar
 
   cd ../setcurrentscene && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setcurrentsceneaction-*.jar ../$FOLD/obssuite_setcurrentsceneaction.jar
+  mv target/obssuite_setcurrentsceneaction-*.jar ../../$FOLD/obssuite_setcurrentsceneaction.jar
 
   cd ../setcurrenttransition && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setcurrenttransitionaction-*.jar ../$FOLD/obssuite_setcurrenttransitionaction.jar
+  mv target/obssuite_setcurrenttransitionaction-*.jar ../../$FOLD/obssuite_setcurrenttransitionaction.jar
 
   cd ../setmute && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setmuteaction-*.jar ../$FOLD/obssuite_setmuteaction.jar
+  mv target/obssuite_setmuteaction-*.jar ../../$FOLD/obssuite_setmuteaction.jar
 
   cd ../setcurrentprofile && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setcurrentprofileaction-*.jar ../$FOLD/obssuite_setcurrentprofileaction.jar
+  mv target/obssuite_setcurrentprofileaction-*.jar ../../$FOLD/obssuite_setcurrentprofileaction.jar
 
   cd ../setpreviewscene && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setpreviewsceneaction-*.jar ../$FOLD/obssuite_setpreviewsceneaction.jar
+  mv target/obssuite_setpreviewsceneaction-*.jar ../../$FOLD/obssuite_setpreviewsceneaction.jar
 
   cd ../setrecording && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setrecordingaction-*.jar ../$FOLD/obssuite_setrecordingaction.jar
+  mv target/obssuite_setrecordingaction-*.jar ../../$FOLD/obssuite_setrecordingaction.jar
 
   cd ../setreplaybuffer && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setreplaybufferaction-*.jar ../$FOLD/obssuite_setreplaybufferaction.jar
+  mv target/obssuite_setreplaybufferaction-*.jar ../../$FOLD/obssuite_setreplaybufferaction.jar
 
   cd ../setstreaming && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setstreamingaction-*.jar ../$FOLD/obssuite_setstreamingaction.jar
+  mv target/obssuite_setstreamingaction-*.jar ../../$FOLD/obssuite_setstreamingaction.jar
 
   cd ../setstudiomode && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setstudiomodeaction-*.jar ../$FOLD/obssuite_setstudiomodeaction.jar
+  mv target/obssuite_setstudiomodeaction-*.jar ../../$FOLD/obssuite_setstudiomodeaction.jar
 
   cd ../setvolume && mvn clean -Dmaven.test.skip package
-  mv target/obssuite_setvolumeaction-*.jar ../$FOLD/obssuite_setvolumeaction.jar
+  mv target/obssuite_setvolumeaction-*.jar ../../$FOLD/obssuite_setvolumeaction.jar
   popd || exit
 }
 
 playaudioclip() {
   pushd . || exit
   cd playaudioclipaction && mvn clean -Dmaven.test.skip package
-  mv target/playaudioclipaction-*.jar $FOLD/playaudioclipaction.jar
+  mv target/playaudioclipaction-*.jar ../$FOLD/playaudioclipaction.jar
   popd || exit
 }
 
 runcommand() {
   pushd . || exit
   cd runcommandaction && mvn clean -Dmaven.test.skip package
-  mv target/runcommandaction-*.jar $FOLD/runcommandaction.jar
+  mv target/runcommandaction-*.jar ../$FOLD/runcommandaction.jar
   popd || exit
 }
 
 textblock() {
   pushd . || exit
   cd textblockaction && mvn clean -Dmaven.test.skip package
-  mv target/textblockaction-*.jar $FOLD/textblockaction.jar
+  mv target/textblockaction-*.jar ../$FOLD/textblockaction.jar
   popd || exit
 }
 
 twitter() {
   pushd . || exit
   cd twitteraction && mvn clean -Dmaven.test.skip package
-  mv target/twitteraction-*.jar $FOLD/twitteraction.jar
+  mv target/twitteraction-*.jar ../$FOLD/twitteraction.jar
   popd || exit
 }
 
 twitchchat() {
   pushd . || exit
   cd twitch/twitch-chat-connect && mvn clean install -Dmaven.test.skip package
-  mv target/twitch-chat-connect-*.jar ../$FOLD/twitch-chat-connect.jar
+  mv target/twitch-chat-connect-*.jar ../../$FOLD/twitch-chat-connect.jar
 
   cd ../send-channel-msg && mvn clean -Dmaven.test.skip package
-  mv target/twitch-send-channel-msg-*.jar ../$FOLD/twitch-send-channel-msg.jar
+  mv target/twitch-send-channel-msg-*.jar ../../$FOLD/twitch-send-channel-msg.jar
 
   cd ../clear-chat && mvn clean -Dmaven.test.skip package
-  mv target/twitch-clear-chat-*.jar ../$FOLD/twitch-clear-chat.jar
+  mv target/twitch-clear-chat-*.jar ../../$FOLD/twitch-clear-chat.jar
 
   cd ../set-color && mvn clean -Dmaven.test.skip package
-  mv target/twitch-set-color-*.jar ../$FOLD/twitch-set-color.jar
+  mv target/twitch-set-color-*.jar ../../$FOLD/twitch-set-color.jar
 
   cd ../whisper && mvn clean -Dmaven.test.skip package
-  mv target/twitch-whisper-*.jar ../$FOLD/twitch-whisper.jar
+  mv target/twitch-whisper-*.jar ../../$FOLD/twitch-whisper.jar
 
   cd ../unraid && mvn clean -Dmaven.test.skip package
-  mv target/twitch-unraid-*.jar ../$FOLD/twitch-unraid.jar
+  mv target/twitch-unraid-*.jar ../../$FOLD/twitch-unraid.jar
 
   cd ../unhost && mvn clean -Dmaven.test.skip package
-  mv target/twitch-unhost-*.jar ../$FOLD/twitch-unhost.jar
+  mv target/twitch-unhost-*.jar ../../$FOLD/twitch-unhost.jar
 
   cd ../add-stream-marker && mvn clean -Dmaven.test.skip package
-  mv target/twitch-add-stream-marker-*.jar ../$FOLD/twitch-add-stream-marker.jar
+  mv target/twitch-add-stream-marker-*.jar ../../$FOLD/twitch-add-stream-marker.jar
 
   cd ../host-channel && mvn clean -Dmaven.test.skip package
-  mv target/twitch-host-channel-*.jar ../$FOLD/twitch-host-channel.jar
+  mv target/twitch-host-channel-*.jar ../../$FOLD/twitch-host-channel.jar
 
   cd ../raid-channel && mvn clean -Dmaven.test.skip package
-  mv target/twitch-raid-channel-*.jar ../$FOLD/twitch-raid-channel.jar
+  mv target/twitch-raid-channel-*.jar ../../$FOLD/twitch-raid-channel.jar
 
   cd ../start-commercial && mvn clean -Dmaven.test.skip package
-  mv target/twitch-start-commercial-*.jar ../$FOLD/twitch-start-commercial.jar
+  mv target/twitch-start-commercial-*.jar ../../$FOLD/twitch-start-commercial.jar
   popd || exit
 }
 
 website() {
   pushd . || exit
   cd websiteaction && mvn clean -Dmaven.test.skip package
-  mv target/websiteaction-*.jar $FOLD/websiteaction.jar
+  mv target/websiteaction-*.jar ../$FOLD/websiteaction.jar
   popd || exit
 }
+
+
+
+
+mkdir -p $FOLD
+rm -rf "${FOLD:?}/"*
+
+cp $DEPS/* $FOLD/
+
 
 case "$1" in
 hotkey)
