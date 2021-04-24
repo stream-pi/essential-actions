ba9d9d373a5750951b745d72aca5220ffbddf4a1
EVNPC <39675973+EVNPC@users.noreply.github.com>
2021-04-24 22:36:08 +0100

    New Changes

--- iphoneairplay/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.stream_pi.iphoneairplay
-{
-    requires com.stream_pi.action_api;
-    requires org.kordamp.ikonli.javafx;
-
-    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with com.stream_pi.iphoneairplay.iPhoneAirplay;
-}
