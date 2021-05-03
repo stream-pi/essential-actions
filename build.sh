426472ae893a23b0bd2194f3caaf3e958f6e152a
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2021-05-04 00:08:07 +0800

    Use mvn wrapper
    Pin mvn version to 3.6.3

--- build.sh
+++ build.sh
@@ -5,118 +5,118 @@ DEPS=Dependencies
 
 hotkey() {
   pushd . || exit
-  cd hotkeyaction && mvn clean -Dmaven.test.skip package
+  cd hotkeyaction && ../mvnw clean -Dmaven.test.skip package
   mv target/hotkeyaction-*.jar ../$FOLD/hotkeyaction.jar
   popd || exit
 }
 
 media_key() {
   pushd . || exit
-  cd mediakeyaction && mvn clean -Dmaven.test.skip package
+  cd mediakeyaction && ../mvnw clean -Dmaven.test.skip package
   mv target/mediakeyaction-*.jar ../$FOLD/mediakeyaction.jar
   popd || exit
 }
 
 obs_suite() {
   pushd . || exit
-  cd obssuite/mother && mvn clean install -Dmaven.test.skip package
+  cd obssuite/mother && ../../mvnw clean install -Dmaven.test.skip package
   mv target/obssuite_motheraction-*.jar ../../$FOLD/obssuite_motheraction.jar
 
-  cd ../setcurrentprofile && mvn clean -Dmaven.test.skip package
+  cd ../setcurrentprofile && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setcurrentprofileaction-*.jar ../../$FOLD/obssuite_setcurrentprofileaction.jar
 
-  cd ../setcurrentscene && mvn clean -Dmaven.test.skip package
+  cd ../setcurrentscene && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setcurrentsceneaction-*.jar ../../$FOLD/obssuite_setcurrentsceneaction.jar
 
-  cd ../setcurrenttransition && mvn clean -Dmaven.test.skip package
+  cd ../setcurrenttransition && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setcurrenttransitionaction-*.jar ../../$FOLD/obssuite_setcurrenttransitionaction.jar
 
-  cd ../setmute && mvn clean -Dmaven.test.skip package
+  cd ../setmute && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setmuteaction-*.jar ../../$FOLD/obssuite_setmuteaction.jar
 
-  cd ../setcurrentprofile && mvn clean -Dmaven.test.skip package
+  cd ../setcurrentprofile && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setcurrentprofileaction-*.jar ../../$FOLD/obssuite_setcurrentprofileaction.jar
 
-  cd ../setpreviewscene && mvn clean -Dmaven.test.skip package
+  cd ../setpreviewscene && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setpreviewsceneaction-*.jar ../../$FOLD/obssuite_setpreviewsceneaction.jar
 
-  cd ../setrecording && mvn clean -Dmaven.test.skip package
+  cd ../setrecording && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setrecordingaction-*.jar ../../$FOLD/obssuite_setrecordingaction.jar
 
-  cd ../setreplaybuffer && mvn clean -Dmaven.test.skip package
+  cd ../setreplaybuffer && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setreplaybufferaction-*.jar ../../$FOLD/obssuite_setreplaybufferaction.jar
 
-  cd ../setstreaming && mvn clean -Dmaven.test.skip package
+  cd ../setstreaming && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setstreamingaction-*.jar ../../$FOLD/obssuite_setstreamingaction.jar
 
-  cd ../setstudiomode && mvn clean -Dmaven.test.skip package
+  cd ../setstudiomode && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setstudiomodeaction-*.jar ../../$FOLD/obssuite_setstudiomodeaction.jar
 
-  cd ../setvolume && mvn clean -Dmaven.test.skip package
+  cd ../setvolume && ../../mvnw clean -Dmaven.test.skip package
   mv target/obssuite_setvolumeaction-*.jar ../../$FOLD/obssuite_setvolumeaction.jar
   popd || exit
 }
 
 play_audio_clip() {
   pushd . || exit
-  cd playaudioclipaction && mvn clean -Dmaven.test.skip package
+  cd playaudioclipaction && ../mvnw clean -Dmaven.test.skip package
   mv target/playaudioclipaction-*.jar ../$FOLD/playaudioclipaction.jar
   popd || exit
 }
 
 run_command() {
   pushd . || exit
-  cd runcommandaction && mvn clean -Dmaven.test.skip package
+  cd runcommandaction && ../mvnw clean -Dmaven.test.skip package
   mv target/runcommandaction-*.jar ../$FOLD/runcommandaction.jar
   popd || exit
 }
 
 text_block() {
   pushd . || exit
-  cd textblockaction && mvn clean -Dmaven.test.skip package
+  cd textblockaction && ../mvnw clean -Dmaven.test.skip package
   mv target/textblockaction-*.jar ../$FOLD/textblockaction.jar
   popd || exit
 }
 
 twitch_chat() {
   pushd . || exit
-  cd twitch/twitch-chat-connect && mvn clean install -Dmaven.test.skip package
+  cd twitch/twitch-chat-connect && ../../mvnw clean install -Dmaven.test.skip package
   mv target/twitch-chat-connect-*.jar ../../$FOLD/twitch-chat-connect.jar
 
-  cd ../send-channel-msg && mvn clean -Dmaven.test.skip package
+  cd ../send-channel-msg && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-send-channel-msg-*.jar ../../$FOLD/twitch-send-channel-msg.jar
 
-  cd ../clear-chat && mvn clean -Dmaven.test.skip package
+  cd ../clear-chat && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-clear-chat-*.jar ../../$FOLD/twitch-clear-chat.jar
 
-  cd ../set-color && mvn clean -Dmaven.test.skip package
+  cd ../set-color && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-set-color-*.jar ../../$FOLD/twitch-set-color.jar
 
-  cd ../whisper && mvn clean -Dmaven.test.skip package
+  cd ../whisper && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-whisper-*.jar ../../$FOLD/twitch-whisper.jar
 
-  cd ../unraid && mvn clean -Dmaven.test.skip package
+  cd ../unraid && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-unraid-*.jar ../../$FOLD/twitch-unraid.jar
 
-  cd ../unhost && mvn clean -Dmaven.test.skip package
+  cd ../unhost && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-unhost-*.jar ../../$FOLD/twitch-unhost.jar
 
-  cd ../add-stream-marker && mvn clean -Dmaven.test.skip package
+  cd ../add-stream-marker && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-add-stream-marker-*.jar ../../$FOLD/twitch-add-stream-marker.jar
 
-  cd ../host-channel && mvn clean -Dmaven.test.skip package
+  cd ../host-channel && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-host-channel-*.jar ../../$FOLD/twitch-host-channel.jar
 
-  cd ../raid-channel && mvn clean -Dmaven.test.skip package
+  cd ../raid-channel && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-raid-channel-*.jar ../../$FOLD/twitch-raid-channel.jar
 
-  cd ../start-commercial && mvn clean -Dmaven.test.skip package
+  cd ../start-commercial && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-start-commercial-*.jar ../../$FOLD/twitch-start-commercial.jar
 
-  cd ../subs-only && mvn clean -Dmaven.test.skip package
+  cd ../subs-only && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-subs-only-*.jar ../../$FOLD/twitch-subs-only.jar
 
-  cd ../slow-mode && mvn clean -Dmaven.test.skip package
+  cd ../slow-mode && ../../mvnw clean -Dmaven.test.skip package
   mv target/twitch-slow-mode-*.jar ../../$FOLD/twitch-slow-mode.jar
 
   popd || exit
@@ -124,14 +124,14 @@ twitch_chat() {
 
 twitter() {
   pushd . || exit
-  cd twitteraction && mvn clean -Dmaven.test.skip package
+  cd twitteraction && ../mvnw clean -Dmaven.test.skip package
   mv target/twitteraction-*.jar ../$FOLD/twitteraction.jar
   popd || exit
 }
 
 website() {
   pushd . || exit
-  cd websiteaction && mvn clean -Dmaven.test.skip package
+  cd websiteaction && ../mvnw clean -Dmaven.test.skip package
   mv target/websiteaction-*.jar ../$FOLD/websiteaction.jar
   popd || exit
 }
