47e372b8e0e7cb0e07c0676c3fae77f2bff337b1
quimodotcom <74259955+quimodotcom@users.noreply.github.com>
2021-05-29 13:58:15 +0100

    fixes

--- phillipshue/changebrightness/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.stream_pi.phillipshue
-{
-    requires com.stream_pi.action_api;
-    requires org.kordamp.ikonli.javafx;
-
-    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with com.stream_pi.phillipshue.changebrightness.ChangeBrightness;
-}
