9d9d27f1d09f93c1d48d3d0cd551f14a9621ff50
EVNPC <39675973+EVNPC@users.noreply.github.com>
2021-05-30 19:31:31 +0100

    fixes

--- documentopen/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.stream_pi.documentopen
-{
-    requires com.stream_pi.action_api;
-    requires org.kordamp.ikonli.javafx;
-
-    requires java.desktop;
-    
-    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with com.stream_pi.documentopen.DocumentOpen;
-}
--- obssuite/setcurrentprofile/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.stream_pi.obssuite.setcurrentprofileaction
-{
-    requires com.stream_pi.action_api;
-    requires com.stream_pi.util;
-
-    requires com.stream_pi.obssuite.motheraction;
-    
-    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with setcurrentprofile.SetCurrentProfile;
-}
