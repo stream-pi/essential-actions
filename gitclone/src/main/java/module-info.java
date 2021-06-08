fa28cbb4f45e54da7f1507964f6d227ee5331d81
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-08 10:49:22 +0530

    Removed gitclone from essential actions

--- gitclone/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.stream_pi.gitclone
-{
-    requires com.stream_pi.action_api;
-    requires org.kordamp.ikonli.javafx;
-    requires org.eclipse.jgit;
-    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with com.stream_pi.gitclone.GitClone;
-}
