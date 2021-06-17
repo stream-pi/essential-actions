534cf02feac67683c5cfe9301800bea26bb1cae4
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2021-06-17 23:38:25 +0800

    Add emote-only twitch chat action

---
+++ twitch/emote-only/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.stream_pi.twitch.emoteonlyaction {
+    requires com.stream_pi.util;
+    requires com.stream_pi.action_api;
+
+    requires com.stream_pi.twitchchatconnectaction;
+    requires twitch.api.java;
+
+    provides com.stream_pi.action_api.externalplugin.ExternalPlugin with emote_only.EmoteOnlyAction;
+}
