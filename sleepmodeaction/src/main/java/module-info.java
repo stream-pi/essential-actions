29c45b3298ef97ff527a225b7af1bd15e33befae
quimodotcom <evandonald01@gmail.com>
2021-07-06 19:34:11 +0100

    Update module-info.java

--- sleepmodeaction/src/main/java/module-info.java
+++ sleepmodeaction/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.stream_pi.sleepmodeaction
 {
     requires com.stream_pi.action_api;
     requires org.kordamp.ikonli.javafx;
-
-    requires java.prefs;
     
     provides com.stream_pi.action_api.externalplugin.ExternalPlugin with com.stream_pi.sleepmodeaction.SleepModeAction;
 }
