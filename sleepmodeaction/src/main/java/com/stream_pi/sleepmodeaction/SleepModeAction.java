94643ed13952baf8df6f325318327474b2f165c6
quimodotcom <74259955+quimodotcom@users.noreply.github.com>
2021-07-06 19:31:12 +0100

    Optimisations

--- sleepmodeaction/src/main/java/com/stream_pi/sleepmodeaction/SleepModeAction.java
+++ sleepmodeaction/src/main/java/com/stream_pi/sleepmodeaction/SleepModeAction.java
@@ -1,14 +1,10 @@
 package com.stream_pi.sleepmodeaction;
 
-import com.stream_pi.action_api.actionproperty.property.*;
 import com.stream_pi.action_api.externalplugin.NormalAction;
+import com.stream_pi.util.platform.Platform;
 import com.stream_pi.util.exception.MinorException;
-import com.stream_pi.util.alert.StreamPiAlert;
-import com.stream_pi.util.alert.StreamPiAlertType;
 import com.stream_pi.util.version.Version;
 
-import java.io.IOException;
-import java.util.prefs.Preferences;
 import java.io.OutputStream;
 
 public class SleepModeAction extends NormalAction
@@ -19,7 +15,6 @@ public class SleepModeAction extends NormalAction
         setAuthor("quimodotcom");
         setHelpLink("https://github.com/quimodotcom/essential-actions");
         setVersion(new Version(1,0,0));
-        setServerButtonGraphic("fas-folder-open");
         setCategory("System");
     }
     
@@ -27,33 +22,33 @@ public class SleepModeAction extends NormalAction
     public void onActionClicked() throws MinorException
     {
         try{
-            String os = System.getProperty("os.name").toLowerCase();
-            if(os.contains("os")){
-            Process p = Runtime.getRuntime().exec
-                ("/bin/bash");
-            String command = "osascript -e 'tell application \"System Events\"' " 
-                + " -e \"sleep\" -e 'end tell'";
-        
-            OutputStream stdin = p.getOutputStream();
-            stdin.write( command.getBytes() );
-            stdin.flush();
-            stdin.close();
+            Platform plat;
+            plat = getPlatform();
+            if(null == plat){
+                throw new MinorException("This action does not support " + System.getProperty("os.name"));
             }
-            else if(os.contains("windows"))
-            {
-                try{
-                    Runtime.getRuntime().exec("Rundll32.exe powrprof.dll,SetSuspendState Sleep");
-                } catch (IOException e){    
-                    e.printStackTrace();
-                }
-            }
-            else if(os.contains("unix"))
-            {
-                Runtime.getRuntime().exec("systemctl hibernate");
-            }
-            else
-            {
-                throw new MinorException("This action does not support " + os);
+            else switch (plat) {
+                case MAC:
+                    Process p = Runtime.getRuntime().exec
+                        ("/bin/bash");
+                    String command = "osascript -e 'tell application \"System Events\"' "
+                            + " -e \"sleep\" -e 'end tell'";
+                    OutputStream stdin = p.getOutputStream();
+                    stdin.write( command.getBytes() );
+                    stdin.flush();
+                    stdin.close();
+                    break;
+                case WINDOWS:
+                    try{
+                        Runtime.getRuntime().exec("Rundll32.exe powrprof.dll,SetSuspendState Sleep");
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }   break;
+                case LINUX:
+                    Runtime.getRuntime().exec("systemctl hibernate");
+                    break;
+                default:
+                    throw new MinorException("This action does not support " + System.getProperty("os.name"));
             }
         
         }catch( Exception e ) { 
